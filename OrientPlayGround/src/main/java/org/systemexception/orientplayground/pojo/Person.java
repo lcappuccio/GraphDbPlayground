fee5840923ffd325a18448f7528da8d359c7fd18
Leonardo Cappuccio <leo@systemexception.org>
2015-02-28 22:14:41 +0100

    green: add_vertex

---
+++ OrientPlayGround/src/main/java/org/systemexception/orientplayground/pojo/Person.java
@@ -0,0 +1,74 @@
+/**
+ *
+ * @author leo
+ * @date 28/02/2015 21:25
+ *
+ */
+package org.systemexception.orientplayground.pojo;
+
+import java.util.Objects;
+import java.util.Properties;
+
+public class Person {
+
+	private String name, surname;
+	private int age;
+
+	public Person(String name, String surname, int age) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Person other = (Person) obj;
+		if (!Objects.equals(this.name, other.name)) {
+			return false;
+		}
+		if (!Objects.equals(this.surname, other.surname)) {
+			return false;
+		}
+		if (this.age != other.age) {
+			return false;
+		}
+		return true;
+	}
+
+}
--- OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/TestOrient.java
+++ OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/TestOrient.java
@@ -6,21 +6,24 @@
  */
 package org.systemexception.orientplayground.test;
 
-import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import org.systemexception.orientplayground.pojo.Person;
 
 public class TestOrient {
 
 	private final Logger log = Logger.getLogger(TestOrient.class);
 	OrientGraphFactory orientGraphFactory;
-	Graph orientGraph;
+	OrientGraph orientGraph;
 	private final String dbPath = "target/orientdb";
 	private final File dbFolder = new File(dbPath);
 
@@ -31,11 +34,12 @@ public class TestOrient {
 			dbFolder.delete();
 		}
 		orientGraphFactory = new OrientGraphFactory("plocal:" + dbPath, "admin", "admin");
-		orientGraph = orientGraphFactory.getNoTx();
+		orientGraph = orientGraphFactory.getTx();
 	}
 
 	@After
 	public void tearDown() {
+		orientGraph.drop();
 		orientGraph.shutdown();
 		orientGraphFactory.close();
 		log.info("Deleting test database");
@@ -45,5 +49,20 @@ public class TestOrient {
 	@Test
 	public void has_a_graph_factory() {
 		assertTrue(orientGraphFactory.exists());
+	}
+
+	@Test
+	public void add_vertex() {
+		Person person = new Person("John", "Doe", 30);
+		Vertex vperson = orientGraph.addVertex("class:Person");
+		vperson.setProperty("name", person.getName());
+		vperson.setProperty("surname", person.getSurname());
+		vperson.setProperty("age", person.getAge());
+
+		Iterator<Vertex> vertexIterator = orientGraph.getVertices("name", "John").iterator();
+		while (vertexIterator.hasNext()) {
+			assertTrue(person.getName() == vertexIterator.next().getProperty("name"));
+		}
+
 	}
 }
