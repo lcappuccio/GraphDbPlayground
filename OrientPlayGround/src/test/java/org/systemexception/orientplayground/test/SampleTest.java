bbf36bd1e00ed5f3c110bc9a413bc1090877a75e
Leonardo Cappuccio <leo@systemexception.org>
2015-02-28 20:05:40 +0100

    initial commit

---
+++ OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/SampleTest.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author leo
+ * @date 28/02/2015 20:03
+ *
+ */
+package org.systemexception.orientplayground.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+	@Test
+	public void alwaysTrue() {
+		assertTrue(true);
+	}
+
+}
