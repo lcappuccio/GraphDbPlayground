78bc2d6be6604b86c4d062f64e8e5e851f1ae250
Leonardo Cappuccio <leo@systemexception.org>
2015-03-01 09:00:37 +0100

    refactor addPersonEdge and added javadocs

--- OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/TestOrient.java
+++ OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/TestOrient.java
@@ -70,17 +70,19 @@ public class TestOrient {
 		Person person2 = new Person("Faa", "Wombat", 30);
 		Vertex vperson2 = addPersonVertex(person2);
 		System.out.println("Added record " + vperson2.getId());
-		Edge edge = orientGraph.addEdge(null, vperson1, vperson2, "brothers");
-		// Add a property to the edge, otherwise it will not be created. Nice feature.
-		// see: https://github.com/orientechnologies/orientdb/wiki/Graph-Database-Tinkerpop
-		edge.setProperty("type", "relation");
-		System.out.println("Added edge " + edge.getId());
+		Edge edge = addPersonEdge(vperson1, vperson2);
 		for (Edge e : orientGraph.getEdges()) {
 			System.out.println(e.getProperty("type").toString());
 		}
 		assertTrue(edge.getVertex(Direction.IN).equals(vperson2) && edge.getVertex(Direction.OUT).equals(vperson1));
 	}
 
+	/**
+	 * Creates a vertex for a Person
+	 *
+	 * @param person
+	 * @return
+	 */
 	private Vertex addPersonVertex(Person person) {
 		Vertex vperson = orientGraph.addVertex(null);
 		vperson.setProperty("name", person.getName());
@@ -88,5 +90,21 @@ public class TestOrient {
 		vperson.setProperty("age", person.getAge());
 		System.out.println("Added vertex: " + person.getName());
 		return vperson;
+	}
+
+	/**
+	 * Creates an edge from vperson1 to vperson2
+	 *
+	 * @param vperson1
+	 * @param vperson2
+	 * @return
+	 */
+	private Edge addPersonEdge(Vertex vperson1, Vertex vperson2) {
+		Edge edge = orientGraph.addEdge(null, vperson1, vperson2, "brothers");
+		// Add a property to the edge, otherwise it will not be created. Nice feature.
+		// see: https://github.com/orientechnologies/orientdb/wiki/Graph-Database-Tinkerpop
+		edge.setProperty("type", "relation");
+		System.out.println("Added edge " + edge.getId());
+		return edge;
 	}
 }
