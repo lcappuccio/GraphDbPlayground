1ae1895669da929cc3aa9a5cd865c0d2139e6741
Leonardo Cappuccio <leo@systemexception.org>
2015-03-01 18:14:34 +0100

    unused import

--- OrientPlayGround/src/main/java/org/systemexception/orientplayground/pojo/CsvParser.java
+++ OrientPlayGround/src/main/java/org/systemexception/orientplayground/pojo/CsvParser.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
