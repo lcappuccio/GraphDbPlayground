8f67f8f1902f21bf679c61b4758476d5b0be7079
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-03-26 17:29:41 +0100

    green: add_DISTINCT_vertex

--- OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/OrientReservedKeywords.java
+++ OrientPlayGround/src/test/java/org/systemexception/orientplayground/test/OrientReservedKeywords.java
@@ -11,26 +11,46 @@
  */
 package org.systemexception.orientplayground.test;
 
+import com.tinkerpop.blueprints.IndexableGraph;
+import com.tinkerpop.blueprints.Vertex;
+import java.util.Iterator;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
-import org.systemexception.orientplayground.api.Action;
+import org.junit.Test;
+import org.systemexception.orientplayground.enums.OrientConfiguration;
 import org.systemexception.orientplayground.exception.CsvParserException;
 import org.systemexception.orientplayground.exception.TerritoriesException;
 import org.systemexception.orientplayground.impl.OrientActionImpl;
 
 public class OrientReservedKeywords {
 
-	private static Action sut;
+	private static OrientActionImpl sut;
+	private static IndexableGraph graph;
 	private final static String dbName = "test_keywords";
 
 	@BeforeClass
 	public static void setUp() throws CsvParserException, TerritoriesException {
 		sut = new OrientActionImpl();
 		sut.initialSetup(dbName);
+		graph = sut.getGraph();
 	}
 
 	@AfterClass
 	public static void tearDown() {
+	}
+
+	@Test
+	public void add_DISTINCT_vertex() {
+		Vertex distinctNamedVertex = graph.addVertex(OrientConfiguration.VERTEX_TERRITORY_CLASS.toString());
+		distinctNamedVertex.setProperty(OrientConfiguration.NODE_ID.toString(), "1");
+		distinctNamedVertex.setProperty(OrientConfiguration.NODE_DESC.toString(), "DISTINCT");
+		distinctNamedVertex.setProperty(OrientConfiguration.NODE_TYPE.toString(), "SomeName");
+		Iterable<Vertex> vertexIterable = graph.getVertices(OrientConfiguration.NODE_ID.toString(), "1");
+		Iterator<Vertex> vertexIterator = vertexIterable.iterator();
+		if (vertexIterator.hasNext()) {
+			assertTrue(vertexIterator.next().getProperty(OrientConfiguration.NODE_ID.toString()) == "1");
+		}
 	}
 
 }
