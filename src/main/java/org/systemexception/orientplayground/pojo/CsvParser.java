0218f4539ada1b8d420b65cc9ea4be7121799cff
Leonardo Cappuccio <leo@systemexception.org>
2015-07-18 22:28:41 +0200

    timer in csv loading

--- src/main/java/org/systemexception/orientplayground/pojo/CsvParser.java
+++ src/main/java/org/systemexception/orientplayground/pojo/CsvParser.java
@@ -9,6 +9,8 @@ package org.systemexception.orientplayground.pojo;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.systemexception.logger.api.Logger;
+import org.systemexception.logger.impl.LoggerImpl;
 import org.systemexception.orientplayground.enums.CsvHeaders;
 import org.systemexception.orientplayground.exception.CsvParserException;
 
@@ -21,9 +23,11 @@ import java.util.List;
 
 public class CsvParser {
 
+	private static final Logger logger = LoggerImpl.getFor(CsvParser.class);
 	private List<CSVRecord> records;
 	private final String[] headerMapping = new String[]{CsvHeaders.PARENT_ID.toString(), CsvHeaders.NODE_ID.toString(),
 		CsvHeaders.DESCRIPTION.toString(), CsvHeaders.TYPE.toString()};
+	private final Timer timer = new Timer();
 
 	public CsvParser(String fileName) throws CsvParserException {
 		CSVFormat csvFormat = CSVFormat.RFC4180.withHeader(headerMapping).withSkipHeaderRecord(true);
@@ -31,7 +35,10 @@ public class CsvParser {
 			URL csvUrl = new File(fileName).toURI().toURL();
 			Reader csvReader = new InputStreamReader(csvUrl.openStream(), "UTF-8");
 			CSVParser csvParser = new CSVParser(csvReader, csvFormat);
+			timer.start();
 			records = csvParser.getRecords();
+			timer.end();
+			logger.info("Loaded " + fileName + ": " + timer.durantionInSeconds() + " seconds");
 		} catch (IOException ex) {
 			throw new CsvParserException("Malformed URL\n" + ex.getMessage());
 		}
