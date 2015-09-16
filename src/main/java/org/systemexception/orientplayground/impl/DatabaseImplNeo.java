cfc21e398dd87eef2427e4de3d443dd355f76f52
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-09-16 15:59:04 +0200

    implement database api

---
+++ src/main/java/org/systemexception/orientplayground/impl/DatabaseImplNeo.java
@@ -0,0 +1,57 @@
+package org.systemexception.orientplayground.impl;
+
+import com.tinkerpop.blueprints.Vertex;
+import org.systemexception.orientplayground.api.DatabaseApi;
+import org.systemexception.orientplayground.exception.CsvParserException;
+import org.systemexception.orientplayground.exception.TerritoriesException;
+
+import java.util.List;
+
+/**
+ * $Id$
+ *
+ * @author leo
+ * @date 16/09/15 15:53
+ */
+public class DatabaseImplNeo implements DatabaseApi {
+
+	@Override
+	public void initialSetup(String dbName, String storageType) {
+
+	}
+
+	@Override
+	public void addTerritories(String fileName) throws CsvParserException, TerritoriesException {
+
+	}
+
+	@Override
+	public Vertex getVertexByNodeId(String nodeId) {
+		return null;
+	}
+
+	@Override
+	public List<Vertex> getChildNodesOf(String nodeId) {
+		return null;
+	}
+
+	@Override
+	public Vertex getParentNodeOf(String nodeId) {
+		return null;
+	}
+
+	@Override
+	public void exportDatabase(String exportFileName) {
+
+	}
+
+	@Override
+	public void backupDatabase(String backupFileName) {
+
+	}
+
+	@Override
+	public void drop() {
+
+	}
+}
